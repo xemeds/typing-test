081a992e0b6ca8329d96efa6e76ecede6b0cdaa8
xemeds <xemeds@protonmail.com>
2020-10-15 16:28:10 +0300

    Added the intro ascii art

--- typing.c
+++ typing.c
@@ -7,6 +7,56 @@
 
 #define NUMER_OF_TEXTS 3
 
+#define CLI_RESET "\033[0m"
+#define CLI_BOLD "\033[1m"
+#define CLI_UNDERLINE "\033[4m"
+#define CLI_BOLD_YELLOW "\033[1;33m"
+#define CLI_BOLD_CYAN "\033[1;36m"
+#define CLI_RED "\033[0;31m"
+#define CLI_BOLD_GREEN "\033[1;32m"
+
+#ifdef __linux
+	// Clears the screen for linux
+	void clear() {
+		system("clear");
+	}
+#elif _WIN32
+	// Clears the screen for windows
+	void clear() {
+		system("cls");
+	}
+#endif
+
+// Prints the ASCII art
+void print_art() {
+	printf(CLI_BOLD_YELLOW);
+	printf("\t\t████████╗██╗░░░██╗██████╗░██╗███╗░░██╗░██████╗░\n");
+	printf("\t\t╚══██╔══╝╚██╗░██╔╝██╔══██╗██║████╗░██║██╔════╝░\n");
+	printf("\t\t░░░██║░░░░╚████╔╝░██████╔╝██║██╔██╗██║██║░░██╗░\n");
+	printf("\t\t░░░██║░░░░░╚██╔╝░░██╔═══╝░██║██║╚████║██║░░╚██╗\n");
+	printf("\t\t░░░██║░░░░░░██║░░░██║░░░░░██║██║░╚███║╚██████╔╝\n");
+	printf("\t\t░░░╚═╝░░░░░░╚═╝░░░╚═╝░░░░░╚═╝╚═╝░░╚══╝░╚═════╝░\n");
+	printf("  ░██████╗██████╗░███████╗███████╗██████╗░ ████████╗███████╗░██████╗████████╗\n");
+	printf("  ██╔════╝██╔══██╗██╔════╝██╔════╝██╔══██╗ ╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝\n");
+	printf("  ╚█████╗░██████╔╝█████╗░░█████╗░░██║░░██║ ░░░██║░░░█████╗░░╚█████╗░░░░██║░░░\n");
+	printf("  ░╚═══██╗██╔═══╝░██╔══╝░░██╔══╝░░██║░░██║ ░░░██║░░░██╔══╝░░░╚═══██╗░░░██║░░░\n");
+	printf("  ██████╔╝██║░░░░░███████╗███████╗██████╔╝ ░░░██║░░░███████╗██████╔╝░░░██║░░░\n");
+	printf("  ╚═════╝░╚═╝░░░░░╚══════╝╚══════╝╚═════╝░ ░░░╚═╝░░░╚══════╝╚═════╝░░░░╚═╝░░░\n\n");
+	printf(CLI_RESET);
+}
+
+// Introduction
+void intro() {
+	clear();
+	print_art();
+	printf(CLI_BOLD_CYAN CLI_UNDERLINE "How it works:\n");
+	printf(CLI_RESET CLI_BOLD);
+	printf("TODO\n\n");
+	printf("Press enter to start...\n> ");
+	getchar();
+	printf(CLI_RESET);
+}
+
 // Gets a random text from the texts file
 char *get_text() {
 	// Get the time
@@ -15,17 +65,16 @@ char *get_text() {
 	// Initialize the random number generator
 	srand((unsigned) time(&t));
 
-	// Get a random line number for the text
+	// Get a random line number
 	int random_line_number = rand() % NUMER_OF_TEXTS;
 
 	// Open the file
 	FILE *file = fopen("texts.txt", "r");
 
 	// Check if the file pointer is empty
-	if (file != NULL)
-	{
+	if (file != NULL) {
 		// Create the max read size per line
-	    char line[256];
+		char line[256];
 
 	    // Declare the line number of the file
 	    int line_number = 0;
@@ -34,25 +83,24 @@ char *get_text() {
 	    while (fgets(line, sizeof line, file) != NULL)
 	    {
 	    	// If the current line number is the random line number
-	        if (line_number == random_line_number)
-	        {
+	        if (line_number == random_line_number) {
+
 	        	// Allocate space for a new text
 	        	char *text = malloc(sizeof(line));
 
 	        	// Copy the line to the text
-				strcpy(text, line);
+	        	strcpy(text, line);
 
-				// Close the file
-	            fclose(file);
+	        	// Close the file
+	        	fclose(file);
 
-	            // Return the pointer to the newly allocated text
-	            return text;
+	        	// Return the pointer to the newly allocated text
+	        	return text;
 	        }
 	        // Else
-	        else
-	        {
+	        else {
 	        	// Increment the line number
-	            line_number++;
+	        	line_number++;
 	        }
 	    }
 	}
@@ -64,9 +112,15 @@ int main() {
 	//char string[10];
 	//fgets(string, 10, stdin);
 
-	char *text = get_text();
-	printf("%s", text);
-	free(text);
+	//time_t start = time(NULL);
+	//time_t end = time(NULL);
+	//printf("Time taken: %li", (end - start));
+
+	//char *text = get_text();
+	//printf("%s", text);
+	//free(text);
+
+	intro();
 
 	return 0;
 }
